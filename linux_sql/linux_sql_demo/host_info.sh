cfd64026a231d1ecd4f231474f3c427300b43d35
Nisarg Khandhar <nisargkhandhar2104@gmail.com>
2023-09-15 21:11:33 +0000

    Made changes in host_usage and host_info file to resolve some errors creating the table

--- linux_sql/linux_sql_demo/host_info.sh
+++ linux_sql/linux_sql_demo/host_info.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
- psql_host=$1
- psql_port=$2
- db_name=$3
- psql_user=$4
- psql_password=$5
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+# Check the number of arguments
+if [ "$#" -ne 5 ]; then
+    echo "Usage: $0 <psql_host> <psql_port> <db_name> <psql_user> <psql_password>"
+    exit 1
+fi
 
- # Check the number of arguments
- if [ "$#" -ne 5 ]; then
-     echo "Usage: $0 <psql_host> <psql_port> <db_name> <psql_user> <psql_password>"
-     exit 1
- fi
+# Parse host hardware specifications using Bash commands
+hostname=$(hostname -f)
+cpu_model=$(lscpu | awk -F: '/^Model name/{print $2}' | xargs)
+memory=$(free -m | awk '/Mem/{print $2}' | xargs)
+disk=$(df -h / | awk 'NR==2{print $2}' | xargs)
+cpu_number=$(lscpu | grep "CPU(s):" | awk '{print $2}')
+cpu_architecture=$(lscpu | grep "Architecture" | awk '{print $2}')
+cpu_mhz=$(lscpu | grep "CPU MHz" | awk '{print $3}')
+l2_cache=$(lscpu | grep "L2 cache" | awk '{print $3}')
 
- # Parse host hardware specifications using Bash commands
- hostname=$(hostname -f)
- cpu=$(lscpu | awk -F: '/^Model name/{print $2}' | xargs)
- memory=$(free -m | awk '/Mem/{print $2}' | xargs)
- disk=$(df -h / | awk 'NR==2{print $2}' | xargs)
+# Escape single quotes in CPU model
+cpu_model_escaped=$(echo "$cpu_model" | sed "s/'/''/g")
 
- # Construct the INSERT statement
- insert_stmt="INSERT INTO host_info (hostname, cpu, memory, disk) VALUES ('$hostname', '$cpu', '$memory', '$disk');"
+# Construct the INSERT statement
+insert_stmt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache) VALUES ('$hostname', $cpu_number, '$cpu_architecture', '$cpu_model_escaped', $cpu_mhz, '$l2_cache');"
 
- # Set up the environment variable for the psql command
- export PGPASSWORD=$psql_password
+# Set up the environment variable for the psql command
+export PGPASSWORD=$psql_password
 
- # Execute the INSERT statement through the psql CLI tool
- psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
- exit $?
+# Execute the INSERT statement through the psql CLI tool
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+
+# Check the exit status of the psql command
+if [ $? -eq 0 ]; then
+    echo "Data insertion successful"
+else
+    echo "Data insertion failed"
+fi
+
+exit $?
--- linux_sql/linux_sql_demo/host_usage.sh
+++ linux_sql/linux_sql_demo/host_usage.sh
@@ -1,29 +1,51 @@
 #!/bin/bash
-# Assign CLI arguments to variables
+
+# Setup and validate arguments
 psql_host=$1
 psql_port=$2
 db_name=$3
 psql_user=$4
 psql_password=$5
 
-# Check the number of arguments
+# Check number of arguments
 if [ "$#" -ne 5 ]; then
-    echo "Usage: $0 <psql_host> <psql_port> <db_name> <psql_user> <psql_password>"
+    echo "Illegal number of parameters"
     exit 1
 fi
 
-# Parse server CPU and memory usage data using Bash scripts
-timestamp=$(date +'%Y-%m-%d %H:%M:%S')
+# Save machine statistics in MB and current machine hostname to variables
+vmstat_mb=$(vmstat --unit M)
 hostname=$(hostname -f)
-memory_free=$(free -m | awk '/Mem/{print $4}')
-cpu_idle=$(vmstat 1 2 | tail -1 | awk '{print $15}')
 
-# Construct the INSERT statement using a subquery to get the host_id
-insert_stmt="INSERT INTO host_usage (timestamp, host_id, memory_free, cpu_idle) VALUES ('$timestamp', (SELECT id FROM host_info WHERE hostname='$hostname'), $memory_free, $cpu_idle);"
+# Retrieve hardware specification variables
+# xargs is a trick to trim leading and trailing white spaces
+memory_free=$(echo "$vmstat_mb" | awk '{print $4}' | tail -n1 | xargs)
+cpu_idle=$(echo "$vmstat_mb" | awk '{print $15}' | tail -n1 | xargs)
+cpu_kernel=$(echo "$vmstat_mb" | awk '{print $14}' | tail -n1 | xargs)
+disk_io=$(vmstat -d | awk '{print $10}' | tail -n1 | xargs)
+disk_available=$(df -BM / | awk 'NR==2 {print $4}' | sed 's/[^0-9]*//g')
 
-# Set up the environment variable for the psql command
+# Current time in `2019-11-26 14:40:19` UTC format
+timestamp=$(date -u +'%Y-%m-%d %H:%M:%S')
+
+# Subquery to find matching id in host_info table
+host_id=$(psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -t -c "SELECT id FROM host_info WHERE hostname='$hostname';")
+
+# PSQL command: Inserts server usage data into host_usage table
+# Note: Replace column names and types as per your host_usage table schema
+insert_stmt="INSERT INTO host_usage(timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) VALUES('$timestamp', $host_id, $memory_free, $cpu_idle, $cpu_kernel, $disk_io, $disk_available);"
+
+# Set up env var for psql command
 export PGPASSWORD=$psql_password
 
-# Execute the INSERT statement through the psql CLI tool
+# Insert data into the database
 psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+
+# Check the exit status of the psql command
+if [ $? -eq 0 ]; then
+    echo "Data insertion successful"
+else
+    echo "Data insertion failed"
+fi
+
 exit $?
